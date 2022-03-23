fa0e89401d3aa5a2a01ae5c8ae972b0524023b1e
Maxim <89240935+Maxmosk@users.noreply.github.com>
2022-03-23 16:24:02 +0300

    4-1-dac fixed

--- 4-1-dac.py
+++ 4-1-dac.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO
 
 
 def decimal2binary(value):
-    assert (value <= 255 and value >= 0)
+    assert value <= 255 and value >= 0
     return [int(bit) for bit in bin(value)[2:].zfill(8)]
 
 
@@ -14,9 +14,23 @@ try:
     GPIO.setup(dac, GPIO.OUT)
 
     while True:
-        n = int(input("Please, enter number from 0 to 255: "))
+        in_str = input("Please, enter number from 0 to 255: ")
+
+        assert in_str != "q"
+
+        if not in_str.isnumeric():
+            print("It isn't incorrect value!")
+            continue
+
+        n = int(in_str)
+        if n > 255 or n < 0:
+            print("Value in out of range!")
+            continue
+
         GPIO.output(dac, decimal2binary(n))
-        print(5 * n / 256)
+        print(3.3 * n / 256)
+except AssertionError:
+    pass
 finally:
     GPIO.output(dac, [0] * 8)
     GPIO.cleanup()
